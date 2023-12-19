805f925169a38d364af051abdb50861a76afea26
Dominik Glenz <doglenz@web.de>
2023-12-19 01:02:33 +0100

    day 13, p2

--- 2023d13.py
+++ 2023d13.py
@@ -14,7 +14,7 @@ A = """#.##..##.
 ..##..###
 #....#..#"""
 
-Ab = """.#..####..#
+A = """.#..####..#
 #....##....
 ..##....##.
 #..#....#..
@@ -1323,39 +1323,54 @@ from dataclasses import dataclass
 @dataclass
 class Field:
     data: str
+    distance: int = 0
 
     def __post_init__(self):
         self.contents = [list(row) for row in self.data.splitlines()]
         self.transposed = [list(row) for row in zip(*self.data.splitlines())]
 
     def find(self):
-        res = self.establish_symmetry(self.contents)
+        res = establish_symmetry(self.contents, dst=self.distance)
         if res is None:
-            res = self.establish_symmetry(self.transposed)
+            res = establish_symmetry(self.transposed, dst=self.distance)
             assert res is not None
             self.score = res[0]
         else:
             self.score = res[0]*100
         return self.score
 
-    def establish_symmetry(self, data):
-        for j, (cur, nxt) in enumerate(zip(data, data[1:])):
-            if cur != nxt:
-                continue
+def dist(line1, line2):
+    dst = 0
+    for c1, c2 in zip(line1, line2):
+        if c1 != c2:
+            dst += 1
+    return dst
 
-            same = True
-            for i in range(len(data)):
-                if j-i < 0 or j+1+i > len(data)-1:
-                    continue
-                same = same and data[j+1+i] == data[j-i]
-                if not same:
-                    break
-            else:
+
+assert dist('.#.#..#', '.#.#..#') == 0
+assert dist('.###..#', '.#.#..#') == 1
+    
+def establish_symmetry(data, dst=0):
+    for j, (cur, nxt) in enumerate(zip(data, data[1:])):
+        if dist(cur, nxt) > dst:
+            continue
+
+        total_dist = dist(cur, nxt)
+        same = True
+        for i in range(1, len(data)):
+            if j-i < 0 or j+1+i > len(data)-1:
+                continue
+            same = same and (dist(data[j+1+i], data[j-i]) <= dst)
+            total_dist += dist(data[j+1+i], data[j-i])
+            if not same:
+                break
+        else:
+            if total_dist == dst:
                 return j+1, j+2
 
 
 R = 0
 for i, field in enumerate(A.split('\n\n')):
-    R += Field(field).find()
+    R += Field(field, distance=1).find()
 
 print(R)
